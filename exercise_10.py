4f4bb31dbee8718d066e977ed8624ac051d7f6db
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 21:39:23 +0800

    pseudocode

--- exercise_10.py
+++ exercise_10.py
@@ -6,3 +6,10 @@
 # Create a new list from two list using the following condition
 # Given two list of numbers, write a program to create a new list such that the new list should contain odd
 # numbers from the first list and even numbers from the second list.
+
+# assign list 1 and list 2
+
+# loop and iterate for odd numbers 
+
+# loop and iterate for even numbers 
+        
