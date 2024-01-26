684612d3c5dd796af591e654ab76018a6689d058
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 21:30:33 +0800

    description

---
+++ exercise_10.py
@@ -0,0 +1,8 @@
+# Assignment 5:
+# Do the exercise 1-15 in https://pynative.com/python-basic-exercise-for-beginners/
+# Try do challenge yourself by not checking the "solution"
+
+# Exercise 10:
+# Create a new list from two list using the following condition
+# Given two list of numbers, write a program to create a new list such that the new list should contain odd
+# numbers from the first list and even numbers from the second list.
